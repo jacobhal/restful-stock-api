a95ee6b72a72be293f6b9cec0a3086d023358e8a
Jacob <jackeaik@hotmail.com>
2020-03-05 19:43:28 +0100

    Test

--- app.py
+++ app.py
@@ -5,82 +5,10 @@ import yahoofinanceAPI
 
 app = Flask(__name__)
 
-@app.route('/getinfo', methods=['GET'])
-def info_response():
-    # Retrieve the equity from url parameter
-    equity = request.args.get("equity", None)
-    res = {}
-
-    if not equity:
-        res["ERROR"] = "No equity specified."
-    else:
-        info = yahoofinanceAPI.get_info(equity)
-        res["DATA"] = info
-
-    response = jsonify(res)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response   
-
-@app.route('/search', methods=['GET'])
-def search_response():
-    # Retrieve the equity from url parameter
-    keywords = request.args.get("keywords", None)
-    res = {}
-
-    if not keywords:
-        res["ERROR"] = "No search keywords specified."
-    else:
-        search_results = alphavantageAPI.search(keywords)
-        res["DATA"] = search_results
-
-    response = jsonify(res)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response 
-
-@app.route('/gethistory', methods=['GET'])
-def history_response():
-    # Retrieve the equity from url parameter
-    equity = request.args.get("equity", None)
-    period = request.args.get("period", None)
-    if period is None:
-        period = "max"
-    res = {}
-
-    if not equity:
-        res["ERROR"] = "No equity specified."
-    else:
-        history = yahoofinanceAPI.get_history(equity, period)
-        res["DATA"] = history
-
-    response = jsonify(res)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response  
-
-@app.route('/gethistoryalpha', methods=['GET'])
-def history_response_alpha():
-    # Retrieve the equity from url parameter
-    equity = request.args.get("equity", None)
-    function = request.args.get("function", None)
- 
-    res = {}
-
-    if not equity:
-        res["ERROR"] = "No equity specified."
-    elif not function:
-        res["ERROR"] = "No function specified."
-    else:
-        history = alphavantageAPI.get_history(equity, function)
-        res["DATA"] = history
-
-    response = jsonify(res)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
 # A welcome message to test our server
 @app.route('/')
 def index():
     response = jsonify({'DATA': '<h1>This is a financial stock API</h1>'})
-    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 @app.after_request
