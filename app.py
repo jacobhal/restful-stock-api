b6909ddeaa5f8206b4adcd0736ec5f9c1248995f
Jacob <jackeaik@hotmail.com>
2020-03-05 19:30:48 +0100

    Flask CORS new version

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 # app.py
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
 import alphavantageAPI
 import yahoofinanceAPI
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/getinfo', methods=['GET'])
-@cross_origin()
 def info_response():
     # Retrieve the equity from url parameter
     equity = request.args.get("equity", None)
@@ -26,7 +22,6 @@ def info_response():
     return response   
 
 @app.route('/search', methods=['GET'])
-@cross_origin()
 def search_response():
     # Retrieve the equity from url parameter
     keywords = request.args.get("keywords", None)
@@ -43,7 +38,6 @@ def search_response():
     return response 
 
 @app.route('/gethistory', methods=['GET'])
-@cross_origin()
 def history_response():
     # Retrieve the equity from url parameter
     equity = request.args.get("equity", None)
@@ -63,7 +57,6 @@ def history_response():
     return response  
 
 @app.route('/gethistoryalpha', methods=['GET'])
-@cross_origin()
 def history_response_alpha():
     # Retrieve the equity from url parameter
     equity = request.args.get("equity", None)
@@ -85,7 +78,6 @@ def history_response_alpha():
 
 # A welcome message to test our server
 @app.route('/')
-@cross_origin()
 def index():
     response = jsonify({'DATA': '<h1>This is a financial stock API</h1>'})
     response.headers.add('Access-Control-Allow-Origin', '*')
