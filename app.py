3cd7a84612aaaa52dcdae72af1c9f8752bfa22e4
Jacob <jackeaik@hotmail.com>
2020-04-26 14:11:52 +0200

    SQL pooling changed

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING', None)
-app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
-app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 19
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 19
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
